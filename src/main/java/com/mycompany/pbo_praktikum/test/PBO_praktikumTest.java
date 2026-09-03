794f1b1454046aed89ac9a12786b1afc5afcdbc7
Harits <ryokira.murayama@gmail.com>
2026-09-03 13:29:13 +0800

    PT. push commit

---
+++ src/main/java/com/mycompany/pbo_praktikum/test/PBO_praktikumTest.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.pbo_praktikum.test;
+
+/**
+ *
+ * @author Harits
+ */
+public class PBO_praktikumTest {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        
+    }
+}
